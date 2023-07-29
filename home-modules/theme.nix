1368cff6d6828cde7e574edaf2a6e58fdf50ff13
Danilo Ferreira <~deudz/dev@list.sr.ht>
2023-07-29 00:33:21 -0300

    where are the buttons? oh there they are!

--- home-modules/theme.nix
+++ home-modules/theme.nix
@@ -12,6 +12,9 @@
       "org/gnome/desktop/interface" = {
         color-scheme = "prefer-dark";
       };
+      "org/gnome/desktop/wm/preferences" = {
+        button-layout = ":minimize,maximize,close";
+      };
     };
   };
 }
