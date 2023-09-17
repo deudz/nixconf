b3acde98d2d48ea800bbb2e73615efade0f146af
Danilo Soares <deudzdev@gmail.com>
2023-09-17 18:25:41 -0300

    add some lsp servers

--- packages/nixvim.nix
+++ packages/nixvim.nix
@@ -5,5 +5,35 @@
   programs.nixvim = {
     enable = true;
     colorschemes.gruvbox.enable = true;
+
+    plugins = {
+      # utilities 
+      treesitter.enable = true;
+      which-key.enable = true;
+      telescope.enable = true;
+      lualine.enable = true;
+      nvim-tree.enable = true;
+      
+      # autocompletion
+      lsp = {
+        enable = true;
+	enabledServers = 
+	  [
+            "html"
+	    "cssls"
+	    "tsserver"
+	    "clangd"
+	    "nixd"
+          ];
+      };
+      luasnip.enable = true;
+      nvim-autopairs.enable = true;
+      
+      # cmp stuff
+      nvim-cmp.enable = true;
+      cmp-nvim-lsp.enable = true;
+      cmp-path.enable = true;
+      cmp-cmdline.enable = true;
+    };
   };
 }
