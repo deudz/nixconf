4677d3d54a66e2368d001130782e8b221b2b127b
Danilo Soares <deudzdev@gmail.com>
2023-09-17 18:23:52 -0300

    make neovim the default editor

--- modules/home.nix
+++ modules/home.nix
@@ -9,6 +9,10 @@
   home.username = "dan";
   home.homeDirectory = "/home/dan";
 
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
+
   home.stateVersion = "23.05";
   programs.home-manager.enable = true;
 }
