3cfc0ca573dfe5431053bf8e3fbb77d6f05b812e
Danilo Soares <deudzdev@gmail.com>
2025-05-07 15:28:03 -0300

    move dotfiles to this repo

--- home/dotfiles.nix
+++ home/dotfiles.nix
@@ -1,15 +1,18 @@
 { config, ... }:
 let
-  mkSymlink = location: config.lib.file.mkOutOfStoreSymlink location;
+  dotfilesDir = "/nix/dotfiles/";
+  mkSymlink = location:
+    config.lib.file.mkOutOfStoreSymlink
+      (builtins.toPath (config.home.homeDirectory + dotfilesDir + location));
 in
 {
   # emacs
-  xdg.configFile."emacs/init.el".source = mkSymlink /home/deudz/nix/dotfiles/emacs/init.el;
-  xdg.configFile."emacs/early-init.el".source = mkSymlink /home/deudz/nix/dotfiles/emacs/early-init.el;
+  # xdg.configFile."emacs/init.el".source = mkSymlink emacs/init.el;
+  # xdg.configFile."emacs/early-init.el".source = mkSymlink emacs/early-init.el;
 
   # zsh
-  home.file.".zshrc".source = mkSymlink /home/deudz/nix/dotfiles/zshrc;
+  home.file.".zshrc".source = mkSymlink "zshrc";
 
   # gitconfig
-  home.file.".gitconfig".source = mkSymlink /home/deudz/nix/dotfiles/gitconfig;
+  home.file.".gitconfig".source = mkSymlink "gitconfig";
 }
